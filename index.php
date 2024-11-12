706fb9ad27d07f5466ea705760c8bd0b44476228
Baptiste <baptiste86@hotmail.com>
2024-11-12 18:30:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Baptiste FAU</title>
+</head>
+<body>
+    Hello wolrd !
+</body>
+</html>
